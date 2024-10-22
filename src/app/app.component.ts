feb2f618a3a7577c88ca45e2c39054610f6f0f2b
Huzaifa Decotechs <127252478+panhwar11090@users.noreply.github.com>
2024-10-22 17:26:55 +0500

    forms control like how to access value in teplate driven form and how we use nGMODEL to access the values

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet , FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'template-driven-form';
+
+  @ViewChild('registerationForm') form : NgForm | undefined;
+
+  OnFormSubmitted(){
+    console.log(this.form?.value.firstname);
+    console.log(this.form?.value.dob)
+    console.log(this.form?.value.country)
+
+  }
+  
+
 }
